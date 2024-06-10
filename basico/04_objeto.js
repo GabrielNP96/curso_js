e036d99cd351312b80a5271d3a65e5eaf617447b
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-10 03:39:47 -0300

    obejto básico.

---
+++ basico/04_objeto.js
@@ -0,0 +1,10 @@
+//declarando um objeto literal
+const personagem = {
+    nome : 'Bruce Wayne',
+    codinome : 'Batman',
+    idade : 84,
+    criadores : ['Bobo Kane', 'Bill Finger'],
+    AnoDeCriação : 1939
+}
+
+console.log(`Nome: ${personagem.nome}.\nCodinome: ${personagem.codinome}.\nIdade: ${personagem.idade} anos.\nCriadores:${personagem.criadores}.\nAno de Criação: ${personagem.AnoDeCriação}.`)
