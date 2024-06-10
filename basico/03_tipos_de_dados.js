943b6671cfaed6bc363f8c9524a636f90e297abd
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-10 03:28:38 -0300

    tipos de dados.

---
+++ basico/03_tipos_de_dados.js
@@ -0,0 +1,9 @@
+//number
+const number = 4
+console.log(`4 = ${typeof(number)}`)
+//string
+const  str= 'Hi!'
+console.log(`Hi! = ${typeof(str)}`)
+//boolean
+const bool = true
+console.log(`true = ${bool}`)
