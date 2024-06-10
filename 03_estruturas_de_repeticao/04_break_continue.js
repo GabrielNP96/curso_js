34f32c56d911585d776e829cfc3ec91926bf8460
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-10 04:45:37 -0300

    novos conceitos

---
+++ 03_estruturas_de_repeticao/04_break_continue.js
@@ -0,0 +1,20 @@
+//break
+for(let i = 0; i <= 10; i++) {
+    console.log(i)
+    
+    if( i === 5) {
+        break
+    }
+}
+
+console.log('----')
+
+//continue
+
+for(let i = 0; i <= 20; i++) {
+    if(i < 4) {
+        continue
+    }
+    console.log(i)
+
+}
