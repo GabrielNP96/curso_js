323603118235112967a702559ec12f8dadf0c914
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-11 04:04:27 -0300

    obj

--- 01_basico/04_objeto.js
+++ 01_basico/04_objeto.js
@@ -3,7 +3,7 @@ const personagem = {
     nome : 'Bruce Wayne',
     codinome : 'Batman',
     idade : 84,
-    criadores : ['Bobo Kane', 'Bill Finger'],
+    criadores : ['Bob Kane', 'Bill Finger'],
     AnoDeCriação : 1939,
     FichaDoPersonagem : function() {
         console.log(`Nome: ${this.nome}.\nCodinome: ${this.codinome}.\nIdade: ${this.idade} anos.\nCriadores:${this.criadores}.\nAno de Criação: ${this.AnoDeCriação}.`)//isso é um metodo
